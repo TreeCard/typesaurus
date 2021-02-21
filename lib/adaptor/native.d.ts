1fbc34b008b0bc214f3a5ae640783f56c7ac43ac
Ifiok Jr <ifiokotung@gmail.com>
2021-02-21 17:17:27 +0000

    chore: add lib files to use with react native

---
+++ lib/adaptor/native.d.ts
@@ -0,0 +1,13 @@
+import { FirebaseFirestoreTypes } from '@react-native-firebase/firestore'
+export default function adaptor(): Promise<{
+  firestore: FirebaseFirestoreTypes.Module
+  consts: {
+    DocumentReference: FirebaseFirestoreTypes.DocumentReference<
+      FirebaseFirestoreTypes.DocumentData
+    >
+    Timestamp: typeof FirebaseFirestoreTypes.Timestamp
+    FieldValue: typeof FirebaseFirestoreTypes.FieldValue
+    FieldPath: typeof FirebaseFirestoreTypes.FieldPath
+  }
+}>
+export declare function injectAdaptor(): void
